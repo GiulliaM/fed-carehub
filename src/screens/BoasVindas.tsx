2a424c817310216fca443242fa6234a9c15eb851
GiulliaM <131501527+GiulliaM@users.noreply.github.com>
2026-04-10 20:00:14 -0300

    Update BoasVindas.tsx

--- src/screens/BoasVindas.tsx
+++ src/screens/BoasVindas.tsx
@@ -8,7 +8,7 @@ export default function BoasVindas({ navigation }: any) {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <Image
-          source={require("../../../assets/bandaid-heart.webp")}
+          source={require("../../assets/bandaid-heart.webp")}
           style={styles.logo}
           resizeMode="contain"
         />
